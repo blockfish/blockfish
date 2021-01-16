0bdff18d3311a3def8fe95afd62c48070868869f
Milo Turner <iitalics@gmail.com>
2021-01-16 00:07:35 -0500

    rudimentary AI interface in nodejs

--- blockfish-js/index.js
+++ blockfish-js/index.js
@@ -1,13 +1,23 @@
 const protos = require('./generated/blockfish_pb.js');
 const IPC = require('./src/ipc.js');
+const AI = require('./src/ai.js');
 
 module.exports = {
     protos,
     IPC,
+    AI,
 };
 
-let ipc = IPC.childProcess('blockfish', []);
-ipc.on('recv', resp => {
-    console.log(resp.toObject());
-    ipc.kill();
+let ai = new AI;
+ai.on('init', motd => {
+    console.log("blockfish version: " + ai.version);
+});
+
+let ss = {
+    queue: "LOJI",
+    matrix: "XXXXX XXXXX\nX XXXXXXXX",
+};
+ai.analyze(ss, result => {
+    console.log('result:');
+    console.log(result);
 });
---
+++ blockfish-js/src/ai.js
@@ -0,0 +1,138 @@
+const EventEmitter = require('events');
+const protos = require('../generated/blockfish_pb.js');
+const IPC = require('./ipc.js');
+
+class AI extends EventEmitter {
+    constructor(ipc) {
+        super();
+        Object.assign(this, {
+            ipc: defaultIPC(ipc),
+            version: null,
+            _init: false,
+            _id: 0,
+            _analysisCallbacks: {},
+        });
+        this.ipc.on('recv', this._onRecv.bind(this));
+        this.ipc.on('error', e => this.emit('error', e));
+    }
+
+    kill() {
+        this.ipc.kill();
+    }
+
+    analyze(snapshot, config, callback) {
+        if (config instanceof Function) {
+            callback = config;
+            config = null;
+        }
+        this._analyze(snapshot, config, callback);
+    }
+
+    _analyze(snapshot, config, callback) {
+        if (!this._init) {
+            this.on('init', () => this._analyze(snapshot, config, callback));
+            return;
+        }
+
+        let setConfigCallback = () => {
+            let ana = new protos.Request.Analyze;
+            let id = this._id++;
+            ana.setId(id);
+            ana.setSnapshot(toSnapshotProto(snapshot));
+            let analyzeReq = new protos.Request;
+            analyzeReq.setAnalyze(ana);
+            this.ipc.send(analyzeReq, () => {
+                this._analysisCallbacks[id] = callback;
+            });
+        };
+
+        if (config === null) {
+            setConfigCallback();
+        } else {
+            let setConfigReq = new protos.Request;
+            setConfigReq.setSetConfig(toConfigProto(config));
+            this.ipc.send(setConfigReq, setConfigCallback);
+        }
+    }
+
+    _onRecv(res) {
+        if (res.hasGreeting()) {
+            let greeting = res.getGreeting();
+            this.version = greeting.getVersion();
+            if (!this._init) {
+                this._init = true;
+                this.emit('init');
+            }
+            this.emit('greet', greeting.getMotd());
+        } else if (res.hasFinished()) {
+            let analysis = res.getFinished();
+            let id = analysis.getId();
+            this._analysisCallbacks[id](fromAnalysisProto(analysis));
+            this._analysisCallbacks[id] = null;
+        }
+    }
+}
+
+function defaultIPC(arg) {
+    if (arg === undefined) {
+        arg = AI.DEFAULT_BLOCKFISH_PATH;
+    }
+    if (typeof arg === 'string') {
+        return IPC.childProcess(arg, []);
+    } else {
+        return arg;
+    }
+}
+
+function toConfigProto(arg) {
+    let cfg = new protos.Request.Config;
+    cfg.setKNodes(arg.nodes || 0);
+    return cfg;
+}
+
+function toSnapshotProto(arg) {
+    let ss = new protos.Snapshot;
+    ss.setHold(arg.hold || "");
+    ss.setQueue(arg.queue);
+    let rows = typeof arg.matrix === 'string' ?
+        arg.matrix.split('\n') :
+        arg.matrix.slice(0);
+    ss.setRowsList(rows.reverse());
+    return ss;
+}
+
+function fromAnalysisProto(arg) {
+    let stats = arg.getStats();
+    let suggs = arg.getSuggestionsList();
+    return {
+        suggestions: suggs.map(fromSuggestionProto),
+        statistics: {
+            nodes: stats.getNodes(),
+            iterations: stats.getIters(),
+            timeTaken: stats.getTimeMs() / 1000,
+        },
+    };
+}
+
+function fromSuggestionProto(arg) {
+    return {
+        rating: arg.getRating(),
+        inputs: arg.getInputsList().map(fromInputProto),
+    };
+}
+
+function fromInputProto(arg) {
+    switch (arg) {
+    case protos.Input.LEFT: return 'left';
+    case protos.Input.RIGHT: return 'right';
+    case protos.Input.CW: return 'cw';
+    case protos.Input.CCW: return 'ccw';
+    case protos.Input.HOLD: return 'hold';
+    case protos.Input.SD: return 'sd';
+    case protos.Input.HD: return 'hd';
+    }
+}
+
+AI.DEFAULT_BLOCKFISH_PATH = 'blockfish';
+
+module.exports = AI;
